426995bab1c75107c3f313e1be56553f796e7e60
PhazonicRidley <ma13hew@gmail.com>
2018-03-21 12:21:50 -0700

    added [p]gnulinux
    shows gnu/linux copy pasta

--- addons/memes.py
+++ addons/memes.py
@@ -374,5 +374,16 @@ class Memes:
                 "```Hey YOU. YES YOU!!!! YOU CAN CREATE A DOWNGRADER. JUST like can Plailect , Aurora Wright , hedgeberg and SciresM and Daeken work on a Switch 3.0.2 dongroder!!!!!!!!!!!!!!!!!!!!!!!!!!!\nI mean I got browser acess with my 6 verzions of teh dns and now i want to downgrade to 3.0.0 and as I have browser I can boot reswotched but it doesnt boot teh hebrew lawnchair its Aurora Wright fault, its incompetent and lazy to not develop for 3.0.2 I want dongroder to 3.0.0 and trustzone exploit quick it's not hard ur the devs do it now quick.\nYou just have to hack/reprogram/patch the 3.0.2 bootrom so I can dongrode. Think the comunity. Cmon your hackers you acn do it. And plilect should make swotch gudie safer!!! becuase evryone bricks!!!!! And limaswotch should add wii u rom support native. take notes Aurora Wright !!!!!!!!!!!!!!I WANT Switch 3.0.2 DONGROADER NOW!!!!!!!!!!!!!!!\nI WANT Switch 3.0.2 DONGROADER NOW!!!!!!!!!!!!!!!\nI WANT Switch 3.0.2 DONGROADER NOW!!!!!!!!!!!!!!!\nI WANT Switch 3.0.2 DONGROADER NOW!!!!!!!!!!!!!!!\nI WANT Switch 3.0.2 DONGROADER NOW!!!!!!!!!!!!!!!\nI WANT Switch 3.0.2 DONGROADER NOW!!!!!!!!!!!!!!!\nI WANT Switch 3.0.2 DONGROADER NOW!!!!!!!!!!!!!!!\nI WANT Switch 3.0.2 DONGROADER NOW!!!!!!!!!!!!!!!\nI WANT Switch 3.0.2 DONGROADER NOW!!!!!!!!!!!!!!!\nI WANT Switch 3.0.2 DONGROADER NOW!!!!!!!!!!!!!!!```" 
             )
 
+    @commands.cooldown (rate=1, per=10.0, type=commands.BucketType.channel)
+    @commands.command()
+    async def gnulinux(self, ctx,):
+        """GNU/Linux Copy Pasta"""
+        await ctx.send("""```I'd just like to interject for a moment. What you're referring to as Linux, is in fact, GNU/Linux, or as I've recently taken to calling it, GNU plus Linux. Linux is not an operating system unto itself, but rather another free component of a fully functioning GNU system made useful by the GNU corelibs, shell utilities and vital system components comprising a full OS as defined by POSIX.
+
+Many computer users run a modified version of the GNU system every day, without realizing it. Through a peculiar turn of events, the version of GNU which is widely used today is often called "Linux", and many of its users are not aware that it is basically the GNU system, developed by the GNU Project.
+
+There really is a Linux, and these people are using it, but it is just a part of the system they use. Linux is the kernel: the program in the system that allocates the machine's resources to the other programs that you run. The kernel is an essential part of an operating system, but useless by itself; it can only function in the context of a complete operating system. Linux is normally used in combination with the GNU operating system: the whole system is basically GNU with Linux added, or GNU/Linux. All the so-called "Linux" distributions are really distributions of GNU/Linux.
+```""")
+
 def setup(bot):
     bot.add_cog(Memes(bot))
