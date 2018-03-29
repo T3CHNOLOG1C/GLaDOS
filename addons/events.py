ccc15264b7678826fe6bc52bb48a8e3f64fc9403
Laine <T3CHNOLOG1C@users.noreply.github.com>
2018-03-29 10:55:06 -0400

    potential fix for my code screwup

--- addons/events.py
+++ addons/events.py
@@ -14,7 +14,7 @@ class Events:
         
     async def on_member_join(self, member):
         user = member
-        geturl = await self.bot.get_user_info(user)
+        geturl = await self.bot.get_user_info(user.id)
         emb = discord.Embed(title="Member Joined", colour=discord.Colour.green())
         emb.add_field(name="Member:", value=member.name, inline=True)
         emb.set_thumbnail(url=user.avatar_url)
