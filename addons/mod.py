aa3f2fe87ff0563ebd44de4a7ff2c1d6f2b02f15
T3CHNOLOG1C <T3CHNOLOG1C@users.noreply.github.com>
2018-03-31 23:59:54 -0400

    rewrote portions of mod cmds

--- addons/mod.py
+++ addons/mod.py
@@ -28,7 +28,7 @@ class Moderation:
         
     @commands.has_permissions(kick_members=True)
     @commands.command()
-    async def kick(self, ctx, member, reason=""):
+    async def kick(self, ctx, member, *, reason=""):
         """Kick a member. (Staff Only)"""
         try:
             try:
@@ -36,13 +36,18 @@ class Moderation:
             except IndexError:
                 await ctx.send("Please mention a user.")
                 return
-            dm_msg = "You have been kicked from {} by {} for the following reason:\n{}".format(ctx.guild.name, ctx.message.author, reason)
+            if reason == "":
+                dm_msg="You have been kicked from {}.".format(ctx.guild.name)
+            else:
+                dm_msg = "You have been kicked from {} for the following reason:\n{}".format(ctx.guild.name, reason)
             await self.dm(member, dm_msg)
             await member.kick()
             await ctx.send("I've kicked {}.".format(member))
             emb = discord.Embed(title="Member Kicked", colour=discord.Colour.red())
-            emb.add_field(name="Member:", value=member.name, inline=True)
-            emb.add_field(name="Mod:", value=ctx.message.author.name, inline=True)
+            emb.add_field(name="Member:", value=member, inline=True)
+            emb.add_field(name="Mod:", value=ctx.message.author, inline=True)
+            if reason == "":
+                reason = "No reason specified."
             emb.add_field(name="Reason:", value=reason, inline=True)
             logchannel = self.bot.logs_channel
             await logchannel.send("", embed=emb)
@@ -72,24 +77,26 @@ class Moderation:
                 
     @commands.has_permissions(ban_members=True)
     @commands.command()
-    async def ban(self, ctx, member, reason=""):
+    async def ban(self, ctx, member, *, reason=""):
         """Ban a member. (Staff Only)"""
         owner = ctx.message.guild.owner
         if len(ctx.message.mentions) == 0:
-            if ctx.message.author == owner:
-                await ctx.send("Yes daddy t3ch?")
-            else:
-                await ctx.send("Please mention a user.")
+            await ctx.send("Please mention a user.")
         else:
             try:
                 member = ctx.message.mentions[0]
-                dm_msg = "You have been banned from {} by {} for the following reason:\n{}".format(ctx.guild.name, ctx.message.author, reason)
+                if reason == "":
+                    dm_msg = "You have been banned from {}.".format(ctx.guild.name)
+                else:
+                    dm_msg = "You have been banned from {} for the following reason:\n{}".format(ctx.guild.name, reason)
                 await self.dm(member, dm_msg)
                 await member.ban(delete_message_days=0)
                 await ctx.send("I've banned {}.".format(member))
                 emb = discord.Embed(title="Member Banned", colour=discord.Colour.red())
                 emb.add_field(name="Member:", value=member.name, inline=True)
                 emb.add_field(name="Mod:", value=ctx.message.author.name, inline=True)
+                if reason == "":
+                    reason = "No reason specified."
                 emb.add_field(name="Reason:", value=reason, inline=True)
                 logchannel = self.bot.logs_channel
                 await logchannel.send("", embed=emb)
@@ -112,22 +119,29 @@ class Moderation:
                 await ctx.send("Banned {}.".format(member))
                 log_msg = ":hammer::hammer::hammer: Multi-ban by {} has banned {} for the following reason: {}".format(ctx.message.author, member, reason)
                 logchannel = self.bot.logs_channel
-                await logchannel.send(log_msg)
+                await logchannel.send(log_msg)  
             except discord.errors.Forbidden:
                 await ctx.send("💢 Couldn't ban {}".format(member))
 
     @commands.has_permissions(manage_messages=True)
     @commands.command()
-    async def lockdown(self, ctx, reason=""):
+
+    async def lockdown(self, ctx, *, reason=""):
         """
         Lock down a channel
         """
         channel = ctx.channel
         await channel.set_permissions(ctx.guild.default_role, send_messages=False)
-        await channel.send(":lock: Channel locked. The given reason is: {}".format(reason))
+        if reason == "":
+            await channel.send(":lock: Channel locked.")
+        else:
+            await channel.send(":lock: Channel locked. The given reason is: {}".format(reason))
         emb = discord.Embed(title="Lockdown", colour=discord.Colour.gold())
-        emb.add_field(name="Channel:", value=ctx.channel.name, inline=True)
-        emb.add_field(name="Mod:", value=ctx.message.author.name, inline=True)
+        ch = "{} ({})".format(ctx.channel.mention, ctx.channel.name)
+        emb.add_field(name="Channel:", value=ch, inline=True)
+        emb.add_field(name="Mod:", value=ctx.message.author, inline=True)
+        if reason == "":
+            reason = "No reason specified."
         emb.add_field(name="Reason:", value=reason, inline=True)
         logchannel = self.bot.logs_channel
         await logchannel.send("", embed=emb)
@@ -143,7 +157,8 @@ class Moderation:
         await channel.set_permissions(ctx.guild.default_role, send_messages=True)
         await channel.send(":unlock: Channel Unlocked")
         emb = discord.Embed(title="Unlock", colour=discord.Colour.gold())
-        emb.add_field(name="Channel:", value=ctx.channel.name, inline=True)
+        ch = "{} ({})".format(ctx.channel.mention, ctx.channel.name)
+        emb.add_field(name="Channel:", value=ch, inline=True)
         emb.add_field(name="Mod:", value=ctx.message.author.name, inline=True)
         logchannel = self.bot.logs_channel
         await logchannel.send("", embed=emb)
@@ -158,9 +173,9 @@ class Moderation:
         Warn members. (Staff Only)
         A user ID can be used instead of mentionning the user.
         - First warn : nothing happens, a simple warning
-        - Second warn : kick
-        - Third warn : muted for a day
-        - Fourth warn : time banned for 3 days
+        - Second warn : muted until an the admin who issued the warning decides to unmute the user.
+        - Third warn : kicked
+        - Fourth warn : kicked
         - Fifth warn : banned
         """
         author = ctx.message.author
@@ -197,29 +212,34 @@ class Moderation:
             "author_id": author.id,
         })
         await ctx.send("🚩 I've warned {}. The user now has {} warns.".format(member, amount_of_warns))
-        await self.dm(member, "You have been warned in {} for the following reason :\n\n{}\n\n".format(ctx.guild.name, reason))
+        if reason == "":
+            await self.dm(member, "You have been warned in {}.".format(ctx.guild.name))
+        else:
+            await self.dm(member, "You have been warned in {} for the following reason :\n{}\n".format(ctx.guild.name, reason))
         emb = discord.Embed(title="Member Warned", colour=discord.Colour.orange())
-        emb.add_field(name="Member:", value=member.name, inline=True)
+        emb.add_field(name="Member:", value=member, inline=True)
         emb.add_field(name="Warning Number:", value=amount_of_warns, inline=True)
-        emb.add_field(name="Mod:", value=ctx.message.author.name, inline=True)
+        emb.add_field(name="Mod:", value=ctx.message.author, inline=True)
+        if reason == "":
+            reason = "No reason specified."
         emb.add_field(name="Reason:", value=reason, inline=True)
         logchannel = self.bot.logs_channel
         await logchannel.send("", embed=emb)
 
         if amount_of_warns == 1:
-            await self.dm(member, "This is your first warning. The next warning will automatically kick you from the server.")
+            await self.dm(member, "This is your first warning. The next warning will automatically mute you.")
         elif amount_of_warns == 2:
-            await self.dm(member, "This is your second warning, so you've been kicked from the server. You can rejoin immediately, but please note that *the next warn will result in an automatic mute!*")
-            await member.kick(reason="Second warn.")
+            await self.dm(member, "This is your second warning, so you've been muted. You will be unmuted whenever the admin who warned you decides to unmute you.\n You will be DM'ed when a mod unmutes you.\n**Do not ask mods to unmute you, as doing so might extend the duration of the mute**")
+            await self.dm(member, "Your next warn will result in being kicked from the server.")
+            await member.add_roles(self.bot.muted_role)
         elif amount_of_warns == 3:
-            await self.dm(member, "This is your third warning, so you are muted for 24 hours. Please note that **the next warn will result in an automatic temporary ban!")
-            # Someone implement a mute command already
+            await self.dm(member, "This is your third warning, so you have been kicked. Please note that **the next warn will result in another kick!**")
+            await member.kick(reason="Third warn")
         elif amount_of_warns == 4:
-            await self.dm(member, "This is your fourth and final warning. **__The next ban will result in an automatic permanent ban.__**")
-            # Someone implement a timeban command already
-            await member.ban(delete_message_days=0, reason="Fourth warn.")
+            await self.dm(member, "You have been kicked from the server. This is your fourth and final warning. **__The next warning will result in an automatic permanent ban.__**")
+            await member.kick(reason="Fourth warn")
         elif amount_of_warns >= 5:
-            await self.dm(member, "You have reached your fifth warning. You are now permanently banned from this server. How the fuck did you even get 5 warns lmao")
+            await self.dm(member, "You have reached your fifth warning. You are now permanently banned from this server.")
             await member.ban(delete_message_days=0, reason="Fifth warn.")
 
         with open("database/warns.json", "w") as f:
@@ -278,8 +298,8 @@ class Moderation:
             js.pop(str(member.id))
             await ctx.send("Cleared all of {}'s warns!".format(member.mention))
             emb = discord.Embed(title="Member Warns Cleared", colour=discord.Colour.orange())
-            emb.add_field(name="Member:", value=member.name, inline=True)
-            emb.add_field(name="Mod:", value=ctx.message.author.name, inline=True)
+            emb.add_field(name="Member:", value=member, inline=True)
+            emb.add_field(name="Mod:", value=ctx.message.author, inline=True)
             logchannel = self.bot.logs_channel
             await logchannel.send("", embed=emb)
             with open("database/warns.json", "w") as f:
@@ -298,18 +318,18 @@ class Moderation:
         if self.bot.approved_role not in member.roles:
             try:
                 await member.add_roles(self.bot.approved_role)
-                dm_msg = "You have been approved by {}, welcome to {}!".format(ctx.message.author, ctx.guild.name)
+                dm_msg = "You have been approved. Welcome to {}!".format(ctx.guild.name)
                 await self.dm(member, dm_msg)
-                await ctx.send(":thumbsup: {} has been approved".format(member.name))
+                await ctx.send(":thumbsup: {} has been approved".format(member))
                 emb = discord.Embed(title="Member Approved", colour=discord.Colour.blue())
-                emb.add_field(name="Member:", value=member.name, inline=True)
-                emb.add_field(name="Mod:", value=ctx.message.author.name, inline=True)
+                emb.add_field(name="Member:", value=member, inline=True)
+                emb.add_field(name="Mod:", value=ctx.message.author, inline=True)
                 logchannel = self.bot.logs_channel
                 await logchannel.send("", embed=emb)
             except discord.errors.Forbidden:
                 await ctx.send("💢 I dont have permission to do this.")
         elif self.bot.approved_role in member.roles:
-            await ctx.send("This member is already approved!")
+            await ctx.send("{} is already approved!".format(member))
 
     @commands.has_permissions(manage_roles=True)
     @commands.command()
@@ -335,7 +355,7 @@ class Moderation:
             emb.add_field(name="Member:", value=member, inline=True)
             emb.add_field(name="Mod:", value=ctx.message.author, inline=True)
             if reason == "":
-                emb.add_field(name="Reason:", value="No reason was given.", inline=True)
+                emb.add_field(name="Reason:", value="No reason specified.", inline=True)
             else:
                 emb.add_field(name="Reason:", value=reason, inline=True)
             logchannel = self.bot.logs_channel
