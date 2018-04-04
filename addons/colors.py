04266fac8dc2ab8ba1e4638fcbe75948a9751bd0
Griffin <4n0nym0ush4ck3r666@gmail.com>
2018-04-03 17:22:36 -0700

    Color toggle (not tested)

--- addons/colors.py
+++ addons/colors.py
@@ -5,6 +5,7 @@ import datetime
 import discord
 from discord.ext import commands
 
+
 class Colors:
     """
     Color commands
@@ -13,61 +14,76 @@ class Colors:
     def __init__(self, bot):
         self.bot = bot
         print("{} addon loaded.".format(self.__class__.__name__))
+        colors = [
+            self.bot.green_role,
+            self.bot.blue_role,
+            self.bot.orange_role,
+            self.bot.white_role,
+            self.bot.black_role,
+            self.bot.sand_role,
+            self.bot.pink_role,
+        ]
         
     @commands.command(pass_context=True)
     async def color(self, ctx, color):
         """Choose your colored role."""
         user = ctx.message.author
         await ctx.message.delete()
+        applied_colors = []
+        for color in colors:
+            if color in user.roles:
+                applied_colors.append(color)
+        if len(applied_colors) > 1:
+            return await user.send("You already have a color!")
         if color == "green":
-            if self.bot.green_role in user.roles:
-                await user.remove_roles(self.bot.green_role)
-                await user.send("Color green removed.")
-            else:
+            if not applied_colors:
                 await user.add_roles(self.bot.green_role)
                 await user.send("Color green added.")
-        elif color == "blue":
-            if self.bot.blue_role in user.roles:
-                await user.remove_roles(self.bot.blue_role)
-                await user.send("Color blue removed.")
             else:
+                await user.remove_roles(self.bot.green_role)
+                await user.send("Color green removed.")
+        elif color == "blue":
+            if not applied_colors:
                 await user.add_roles(self.bot.blue_role)
                 await user.send("Color blue added.")
-        elif color == "orange":
-            if self.bot.orange_role in user.roles:
-                await user.remove_roles(self.bot.orange_role)
-                await user.send("Color orange removed.")
             else:
+                await user.remove_roles(self.bot.blue_role)
+                await user.send("Color blue removed.")
+        elif color == "orange":
+            if not applied_colors:
                 await user.add_roles(self.bot.orange_role)
                 await user.send("Color orange added.")
+            else:
+                await user.remove_roles(self.bot.orange_role)
+                await user.send("Color orange removed.")
         elif color == "white":
-            if self.bot.white_role in user.roles:
-                await user.remove_roles(self.bot.white_role)
-                await user.send("Color white removed.")
+            if not applied_colors:
+                await user.add_roles(self.bot.orange_role)
+                await user.send("Color orange added.")
             else:
-                await user.add_roles(self.bot.white_role)
-                await user.send("Color white added.")
+                await user.remove_roles(self.bot.orange_role)
+                await user.send("Color orange removed.")
         elif color == "black":
-            if self.bot.black_role in user.roles:
-                await user.remove_roles(self.bot.black_role)
-                await user.send("Color black removed.")
-            else:
+            if not applied_colors:
                 await user.add_roles(self.bot.black_role)
                 await user.send("Color black added.")
-        elif color == "sand":
-            if self.bot.sand_role in user.roles:
-                await user.remove_roles(self.bot.sand_role)
-                await user.send("Color sand removed.")
             else:
+                await user.remove_roles(self.bot.black_role)
+                await user.send("Color black removed.")
+        elif color == "sand":
+            if not applied_colors:
                 await user.add_roles(self.bot.sand_role)
                 await user.send("Color sand added.")
-        elif color == "pink":
-            if self.bot.pink_role in user.roles:
-                await user.remove_roles(self.bot.pink_role)
-                await user.send("Color pink removed.")
             else:
+                await user.remove_roles(self.bot.sand_role)
+                await user.send("Color sand removed.")
+        elif color == "pink":
+            if not applied_colors:
                 await user.add_roles(self.bot.pink_role)
                 await user.send("Color pink added.")
+            else:
+                await user.remove_roles(self.bot.pink_role)
+                await user.send("Color pink removed.")
         else:
             await user.send("{} is not a togglable color.".format(color))
 
