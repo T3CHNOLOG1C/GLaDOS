6686a627cc0d5678d479e9d805698513e490a39a
sixardyH <cyril.devaucleroy@gmail.com>
2018-07-28 01:25:58 +0200

    Make userinfo usable without self-mentionning

--- addons/misc.py
+++ addons/misc.py
@@ -5,6 +5,7 @@ import datetime
 import discord
 from discord.ext import commands
 
+
 class Misc:
     """
     Miscellaneous commands
@@ -13,7 +14,7 @@ class Misc:
     def __init__(self, bot):
         self.bot = bot
         print("{} addon loaded.".format(self.__class__.__name__))
-        
+
     @commands.command(pass_context=True)
     async def ping(self, ctx):
         """Pong!"""
@@ -27,12 +28,12 @@ class Misc:
     async def membercount(self, ctx):
         """Prints current member count"""
         return await ctx.send("{} currently has {} members!".format(ctx.guild.name, len(ctx.guild.members)))
-    
+
     @commands.command()
     async def about(self, ctx):
         """About GLaDOS."""
         return await ctx.send("View my source code here: https://github.com/T3CHNOLOG1C/GLaDOS")
-        
+
     @commands.has_permissions(manage_messages=True)
     @commands.command()
     async def clear(self, ctx, amount):
@@ -51,12 +52,11 @@ class Misc:
             await ctx.say("💢 I don't have permission to do this.")
 
     @commands.command()
-    async def userinfo(self, ctx, member):
+    async def userinfo(self, ctx):
         try:
             member = ctx.message.mentions[0]
         except IndexError:
-            await ctx.send("Please mention a user.")
-            return
+            member = ctx.message.author
         uname = member.name
         discrim = member.discriminator
         Uid = member.id
@@ -72,7 +72,7 @@ class Misc:
         str4 = "{}".format(str(isBot))
         str5 = "{}".format(cTime)
         str6 = "{}".format(jTime)
-        emb = discord.Embed(title="Userinfo", color= 0x00ff00)
+        emb = discord.Embed(title="Userinfo", color=0x00ff00)
         emb.add_field(name="Member", value=str1 + "\n", inline=True)
         emb.add_field(name="ID", value=str2 + "\n", inline=True)
         emb.add_field(name="Nickname", value=str3 + "\n", inline=True)
@@ -81,6 +81,8 @@ class Misc:
         emb.add_field(name="Joined Server", value=str6 + "\n", inline=True)
         emb.set_thumbnail(url=member.avatar_url)
         await ctx.send("", embed=emb)
-            
+
+
 def setup(bot):
     bot.add_cog(Misc(bot))
+
