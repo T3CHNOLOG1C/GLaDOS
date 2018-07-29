0a4c6106dc0ba4318ea5ac5d683b3c886ee07934
Jan200101 <sentrycraft123@gmail.com>
2018-07-29 21:14:28 +0200

    Cleaned up misc.py and made it pep8 compliant

--- addons/misc.py
+++ addons/misc.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3.6
 
 import datetime
+from os import devnull
+from subprocess import Popen, PIPE
+try:
+    from subprocess import DEVNULL # Python 3
+except ImportError:
+    DEVNULL = open(devnull, 'r+b', 0)
 
 import discord
 from discord.ext import commands
@@ -13,7 +19,7 @@ class Misc:
     def __init__(self, bot):
         self.bot = bot
         print("{} addon loaded.".format(self.__class__.__name__))
-        
+
     @commands.command(pass_context=True)
     async def ping(self, ctx):
         """Pong!"""
@@ -21,18 +27,27 @@ class Misc:
         currtime = datetime.datetime.now()
         latency = currtime - mtime
         ptime = str(latency.microseconds / 1000.0)
-        return await ctx.send(":ping_pong:! Pong! Response time: {} ms".format(ptime))
+        await ctx.send(":ping_pong:! Pong! Response time: {} ms".format(ptime))
+        return
 
     @commands.command(pass_context=True, aliases=['mc'])
     async def membercount(self, ctx):
         """Prints current member count"""
-        return await ctx.send("{} currently has {} members!".format(ctx.guild.name, len(ctx.guild.members)))
-    
+        await ctx.send("{} currently has {} members!"
+                       "".format(ctx.guild.name, len(ctx.guild.members)))
+        return
+
     @commands.command()
     async def about(self, ctx):
         """About GLaDOS."""
-        return await ctx.send("View my source code here: https://github.com/T3CHNOLOG1C/GLaDOS")
-        
+        repo = Popen(["git", "config", "--get", "remote.origin.url"],
+                     stdout=PIPE, stdin=DEVNULL, stderr=DEVNULL).communicate()[0]
+        if repo:
+            await ctx.send("View my source code here: {}".format(repo))
+        else: # use base as fallback incase this isn't a git clone
+            await ctx.send("View my source code here: https://github.com/T3CHNOLOG1C/GLaDOS")
+        return
+
     @commands.has_permissions(manage_messages=True)
     @commands.command()
     async def clear(self, ctx, amount):
@@ -72,7 +87,7 @@ class Misc:
         str4 = "{}".format(str(isBot))
         str5 = "{}".format(cTime)
         str6 = "{}".format(jTime)
-        emb = discord.Embed(title="Userinfo", color= 0x00ff00)
+        emb = discord.Embed(title="Userinfo", color=0x00ff00)
         emb.add_field(name="Member", value=str1 + "\n", inline=True)
         emb.add_field(name="ID", value=str2 + "\n", inline=True)
         emb.add_field(name="Nickname", value=str3 + "\n", inline=True)
@@ -81,6 +96,6 @@ class Misc:
         emb.add_field(name="Joined Server", value=str6 + "\n", inline=True)
         emb.set_thumbnail(url=member.avatar_url)
         await ctx.send("", embed=emb)
-            
+
 def setup(bot):
     bot.add_cog(Misc(bot))
