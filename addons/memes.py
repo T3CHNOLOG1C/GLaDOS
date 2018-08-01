f5dfe0911f26b9910ee60b10a7b2b878820dd7c2
Jan200101 <sentrycraft123@gmail.com>
2018-08-01 22:37:35 +0200

    T3ch gay

--- addons/memes.py
+++ addons/memes.py
@@ -517,5 +517,28 @@ class Memes:
         roast = await commands.clean_content().convert(ctx, roast)
         await ctx.send("Your {} is Ready".format(roast))
 
+    @commands.command()
+    async def t3ch(self, ctx, *, arg="server, sss, weeb, shack mod, trap role, channel, shitposting, cancerous"):
+        """
+        Prints the T3CHNOLOGIC copypasta.
+        With no or missing arguments, defaults to the original pasta (or a modified original).
+        Usage: [p]t3ch [server], [sss], [weeb], [shack mod], [trap role], [channel], [shitposting], [cancerous]
+        """
+        original = ["server", "sss", "weeb", "shack mod", "trap role", "channel", "shitposting",
+                    "cancerous"]
+        replacements = [i.lstrip().rstrip() for i in arg.split(",")]
+        del replacements[len(original):]
+        if len(replacements) < 8:
+            replacements = replacements + original[len(replacements):]
+
+        await ctx.send(
+            await commands.clean_content(
+            ).convert(ctx,
+                      ("y'know, i was trying to keep my cool and be a part of this {0}, but i cant"
+                       " force myself here any longer. this isnt {1}. too much {2} shit, owned by "
+                       "a {3} and a former {3}, no {4}, a {5} where people get warned for {6} when"
+                       " part of the spirit of {1} is {6}, and overall just more {7} than {1} was "
+                       "ever meant to be. goodbye.").format(*replacements)))
+
 def setup(bot):
     bot.add_cog(Memes(bot))
